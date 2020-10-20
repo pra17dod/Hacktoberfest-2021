497aa92f3a69e7fa3b54306183ae93758a99d1c0
Neel Bavarva <neelbavarva5@gmail.com>
2020-10-20 18:44:42 +0530

    app.js removed

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Main from './components/main';
 import 'bootstrap/dist/css/bootstrap.css';
